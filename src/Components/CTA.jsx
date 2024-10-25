dac71d8da24a46fd61c27a94af340be198e8c00c
paula <paulalaorga@gmail.com>
2024-10-26 01:08:17 +0200

    Main component Styled

--- src/Components/CTA.jsx
+++ src/Components/CTA.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import '../index.css';
 
 const CTA = () => {
     return (
-        <div>
-            <h1>Call to Action</h1>
-            <button>Click Me</button>
+        <div className='cta'>
+            <button className='button-cta' href='wwww.ourblog.com'>
+            Our Blog
+            </button>
         </div>
     );
 };
 
 export default CTA;
+
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import '../index.css';
 
-const Card = () => {
+const Card = ({ imageSrc, tag, date, title, type }) => {
+    const tagClassName = type === 'Type B' ? 'tag tag-b' : 'tag';
+
     return (
-        <div className="card">
-            <h2>Card Title</h2>
-            <p>This is a basic card component.</p>
+        <div className='card'>
+            <div className="card-img">
+                <img src={imageSrc} alt={title} />
+                <span className={tagClassName}>{tag}</span>
+            </div>
+            <div className="card-content">
+                <div className="date">{date}</div>
+                <p>{title}</p>
+            </div>
         </div>
     );
 };
