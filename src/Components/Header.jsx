df136bf83243997a63c3dad5c7a1b2bcaa8d30a0
paula <paulalaorga@gmail.com>
2024-10-25 23:45:11 +0200

    Header Styled

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Navbar from './Navbar';
+import '../index.css'
 
 const Header = () => {
     return (
-        <header>
-            <h1>Welcome to My Website</h1>
+        <header className='header'>
+            <img className='logo' src="https://tf-frontend.netlify.app/images/v1/logo.svg" alt="logo"/>
             <Navbar/>
         </header>
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav>
-            <h1>Navbar</h1>
+        <nav class="nav" style={
+            {
+                display: 'flex',
+                gap: '10px',
+                padding: '10px',
+                margin: '10px',
+                fontFamily: 'Arial',
+            }
+        }>
+            <p>Store</p>
+            <p>Our Story</p>
+            <p>Journal</p>
         </nav>
     );
 };
