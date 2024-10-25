202a0fe0d967096e3109f567b3caa6250891f883
paula <paulalaorga@gmail.com>
2024-10-26 01:08:17 +0200

    Main component Styled

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './index.css';
 import Hero from './Components/Hero.jsx';
 import Main from './Components/Main.jsx';
-import CTA from './Components/CTA.jsx';
 import Header from './Components/Header.jsx';
 
 const Home = () => {
@@ -11,7 +10,6 @@ const Home = () => {
     <Header/>
     <Hero/>
     <Main/>
-    <CTA/>
     </>
   );
 };
