848bc8dcb0453ada577122b208ff12de2c39ac66
paula <paulalaorga@gmail.com>
2024-10-25 23:22:08 +0200

    Components base code

---
+++ src/Components/CTA.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CTA = () => {
+    return (
+        <div>
+            <h1>Call to Action</h1>
+            <button>Click Me</button>
+        </div>
+    );
+};
+
+export default CTA;
